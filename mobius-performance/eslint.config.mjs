d64e87a0fc3cece98507b71773ef4f55c1442e36
Henrique Parreira Costa <148904673+enderxdxd@users.noreply.github.com>
2026-02-09 19:26:23 -0800

    fixx: eslint warn

--- mobius-performance/eslint.config.mjs
+++ mobius-performance/eslint.config.mjs
@@ -22,10 +22,10 @@ const eslintConfig = [
   },
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-require-imports": "warn",
-      "import/no-anonymous-default-export": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+      "import/no-anonymous-default-export": "off",
     },
   },
 ];
